3139cb1b9f53ad510de4ffeaf2cfc37ad4c63e29
Abdullah Mansoor <abdullahse001@gmail.com>
2021-05-10 01:17:12 +0530

    Service registration

--- WhetherApi/Startup.cs
+++ WhetherApi/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using WhetherApi.Services;
+using WhetherApi.Contracts;
 
 namespace WhetherApi
 {
@@ -26,6 +28,7 @@ namespace WhetherApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddScoped<IWeatherConverterService, WeatherConverterService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
