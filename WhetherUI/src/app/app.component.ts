e810dde4fdb5a48922f0bdfd9a77dffad346460a
Abdullah Mansoor <abdullahse001@gmail.com>
2021-05-10 02:53:05 +0530

    App Component Modified

--- WhetherUI/src/app/app.component.ts
+++ WhetherUI/src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { TemperatureType } from './enums/temperature-type';
+import { WeatherForecast } from './models/weather-forecast';
+import { WeatherConverterService } from './services/weather-converter.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'WhetherUI';
+
+  type = new FormControl(TemperatureType.Celsius);
+  tempValue = new FormControl(0);
+  weather$: Observable<WeatherForecast>;
+
+  constructor(private weatherConverterService: WeatherConverterService) {
+  }
+
+  convert() {
+    this.weather$ = this.weatherConverterService
+      .getWeatherForcast(this.type.value as TemperatureType, this.tempValue.value);
+  }
 }
